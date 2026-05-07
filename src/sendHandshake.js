30fd555e6fc660d3de54d1d952d5dafc55e7e2d1
Jon Lachlan <jonlachlan@protonmail.com>
2026-05-06 18:39:50 -0700

    Fix WebSocket Handshake HTTP syntax when no extra headers are provided.

--- src/sendHandshake.js
+++ src/sendHandshake.js
@@ -5,33 +5,33 @@
 import { createHash } from 'crypto';
 
 export default function sendHandshake (
-    request /* <http.IncomingMessage> */, 
+    request /* <http.IncomingMessage> */,
     socket /* <stream.Duplex> */,
     headers = [] /* Optional <Array> of well-formatted http header strings */
 ) {
-        
+
     // Global identifier for handshake per RFC 6455
     const guid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11';
-    
+
     // Sec-WebSocket-Accept formation per RFC 6455
     const secWebsocketAccept = createHash('sha1')
         .update(
             `${request.headers['sec-websocket-key']}${guid}`
         )
         .digest('base64');
-    
+
     // send handshake
     socket.write(
         'HTTP/1.1 101 Switching Protocol\r\n' +
-        /* 
-         * Per RFC 6455 Section 4.1, a "fail" should occur when Upgrade is not 
+        /*
+         * Per RFC 6455 Section 4.1, a "fail" should occur when Upgrade is not
          * a case-insensitive match to "websocket"
-        */ 
-        'Upgrade: WebSocket\r\n' + 
+        */
+        'Upgrade: WebSocket\r\n' +
         'Connection: Upgrade\r\n' +
         `Sec-WebSocket-Accept: ${secWebsocketAccept}\r\n` +
-        headers.join('\r\n') + '\r\n'
+        `${headers.length <= 1 && (!headers.length || !headers[0]) ? '' : headers.join('\r\n').concat('\r\n')}`
          + '\r\n'
     );
-        
+
 }
